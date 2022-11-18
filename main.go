b278f841483ca5dfbbf24592f8f28046dc670196
Darren Kellenschwiler <deggen@kschw.com>
2022-11-18 17:21:50 -0600

    something is wrong with how they are being hashed.
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 )
 
 type BlockJson struct {
@@ -14,7 +16,7 @@ type BlockJson struct {
 }
 
 type BlockBinary struct {
-	Txids      [][]byte
+	Txids      [][32]byte
 	Hash       []byte
 	MerkleRoot []byte
 }
@@ -28,9 +30,15 @@ func hexToBytes(h string) ([]byte, error) {
 }
 
 func blockBinaryFromJson(blockJson *BlockJson) (*BlockBinary, error) {
-	txids := make([][]byte, len(blockJson.Txids))
-	for i, txid := range blockJson.Txids {
-		txids[i] = []byte(txid)
+	txids := make([][32]byte, len(blockJson.Txids))
+	for i, hexTxid := range blockJson.Txids {
+		var txid [32]byte
+		hash, err := hexToBytes(hexTxid)
+		if err != nil {
+			return nil, err
+		}
+		copy(txid[:], []byte(hash))
+		txids[i] = txid
 	}
 	hash, err := hexToBytes(blockJson.Hash)
 	if err != nil {
@@ -45,6 +53,11 @@ func blockBinaryFromJson(blockJson *BlockJson) (*BlockBinary, error) {
 		Hash:       hash,
 		MerkleRoot: merkleRoot,
 	}, nil
+}
+
+func H(digest []byte) [32]byte {
+	one := sha256.Sum256(digest)
+	return sha256.Sum256(one[:])
 }
 
 func getBlockFromFile(filename string) (*BlockBinary, error) {
@@ -64,11 +77,69 @@ func getBlockFromFile(filename string) (*BlockBinary, error) {
 	return opti, nil
 }
 
+func calculateMerkleNodes(block *BlockBinary) ([][][32]byte, error) {
+	numberofLevels := int(math.Ceil(math.Log2(float64(len(block.Txids)))))
+	// the nodes are all 32 byte hashes
+	var nodes [][][32]byte
+
+	// the first layer of nodes are just the transactions hashes themselves
+	nodes = append(nodes, block.Txids)
+	for i := 0; i < numberofLevels; i++ {
+		targetNodes := make([][32]byte, 0)
+		nodes = append(nodes, targetNodes)
+	}
+
+	fmt.Println(numberofLevels)
+	// fmt.Println(nodes)
+
+	// if there's only one then that's the only node and it's also the root
+	if len(block.Txids) == 1 {
+		return nodes, nil
+	}
+
+	for level, nodesAtThisLevel := range nodes {
+		if len(nodesAtThisLevel) == 1 {
+			break
+		}
+		if level == len(nodes)-1 {
+			targetNodes := make([][32]byte, 0)
+			nodes = append(nodes, targetNodes)
+		}
+		// fmt.Println(level, len(nodesAtThisLevel))
+		targetLevel := level + 1
+		var visualization string
+		for idx, node := range nodesAtThisLevel {
+			visualization += "-"
+			if (idx % 2) != 0 {
+				digest := append(nodesAtThisLevel[idx-1][:], node[:]...)
+				nodes[targetLevel] = append(nodes[targetLevel], H(digest))
+				b := H(digest)
+				fmt.Println("l", hex.EncodeToString(b[:]))
+				continue
+			}
+			if idx == len(nodesAtThisLevel)-1 {
+				digest := append(node[:], node[:]...)
+				nodes[targetLevel] = append(nodes[targetLevel], H(digest))
+				b := H(digest)
+				fmt.Println("r", hex.EncodeToString(b[:]))
+				continue
+			}
+		}
+		fmt.Println(visualization)
+	}
+	return nodes, nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
 	block, err := getBlockFromFile("data/block.json")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(block)
+
+	nodes, err := calculateMerkleNodes(block)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(hex.EncodeToString(nodes[len(nodes)-1][0][:]))
+	fmt.Println(hex.EncodeToString(block.MerkleRoot))
 }
