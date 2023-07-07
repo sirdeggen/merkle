a94b10072ae4bc925dab5d162e589c284dbaa982
Darren Kellenschwiler <deggen@kschw.com>
2023-07-07 10:30:30 -0500

    all valid without console logging
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- main.go
+++ main.go
@@ -35,7 +35,6 @@ func Read(name string, index uint64) ([]byte, error) {
 	}
 	// convert to uint64
 	numOfTxs := binary.LittleEndian.Uint64(UintBytes)
-	fmt.Println("numOfTxs: ", numOfTxs)
 
 	// is index within
 	if index >= numOfTxs {
@@ -54,7 +53,6 @@ func Read(name string, index uint64) ([]byte, error) {
 	for powerMask < (math.MaxUint64 / 2) {
 		powerMask = (powerMask * 2) | powerMask
 	}
-	fmt.Printf("%64b\n", powerMask)
 
 	// number of branches per level
 	branches := make([]uint64, power+1)
@@ -62,7 +60,6 @@ func Read(name string, index uint64) ([]byte, error) {
 	for x := 1; x <= power; x++ {
 		branches[x] = uint64(math.Ceil(float64(branches[x-1]) / 2))
 	}
-	fmt.Println("branches: ", branches)
 
 	cumulativeBranchOffset := uint64(0)
 	powerOffset := uint64(0)
@@ -71,27 +68,18 @@ func Read(name string, index uint64) ([]byte, error) {
 		mask >>= 1
 		branchOffset := branches[len(branches)-1-x]
 		cumulativeBranchOffset += branchOffset
-		fmt.Printf("%64b\n", mask)
-		fmt.Printf("%64b\n", index)
-		fmt.Printf("%64b\n", index&mask)
 		if index&mask > 0 {
-			fmt.Println("r")
 			powerOffset++
 			// the tx is in the right branch
 			// therefore we read the left path
 			d := make([]byte, 32)
 			_, err = f.Read(d)
-			var t [32]byte
-			copy(t[:], d)
-			hash := helpers.Reverse(t)
-			fmt.Println("hash: ", hex.EncodeToString(hash[:]))
 			if err != nil {
 				return nil, err
 			}
 			data = append(data, d...)
 			// and add one to the offset
 		} else {
-			fmt.Println("l")
 			// the tx is in the left branch
 			// therefore we read the right path by skipping forward one
 			// unless it's the right most, in which case it's a duplicate of the left
@@ -104,10 +92,6 @@ func Read(name string, index uint64) ([]byte, error) {
 			}
 			d := make([]byte, 32)
 			_, err = f.Read(d)
-			var t [32]byte
-			copy(t[:], d)
-			hash := helpers.Reverse(t)
-			fmt.Println("hash: ", hex.EncodeToString(hash[:]))
 			if err != nil {
 				return nil, err
 			}
@@ -118,11 +102,6 @@ func Read(name string, index uint64) ([]byte, error) {
 		powerOffset <<= 1
 		skip = powerOffset + cumulativeBranchOffset
 		seekPosition := (32 * skip) + 8
-		fmt.Println("\n\nNext hash will be read from:")
-		fmt.Println("skip: ", skip)
-		fmt.Println("powerOffset: ", powerOffset)
-		fmt.Println("branchOffset: ", branchOffset)
-		fmt.Println("cumulativeBranchOffset: ", cumulativeBranchOffset)
 		_, err = f.Seek(int64(seekPosition), 0)
 		if err != nil {
 			return nil, err
@@ -154,28 +133,9 @@ func main() {
 		fmt.Println(err)
 	}
 
-	wholeTree, _ := service.CalculateMerkleBranches(block)
-
-	// print each line as hex string
-	for x := len(wholeTree) - 1; x >= 0; x-- {
-		fmt.Println("\nLevel: ", x)
-		for y := 0; y < len(wholeTree[x]); y++ {
-			//data[(x-1)*32:x*32]
-			// reverse the 32 bytes
-			var chunk [32]byte
-			copy(chunk[:], wholeTree[x][y][:])
-			d := helpers.Reverse(chunk)
-			fmt.Print(hex.EncodeToString(d[:6]), " ")
-		}
-	}
-
 	// check all of the merkle paths
 	for x := 0; x < len(block.Txids); x++ {
-		fmt.Println("\n\nIndex: ", x)
 		txid := block.Txids[x]
-		rev := helpers.Reverse(txid)
-		fmt.Println("\nTxid: ", hex.EncodeToString(rev[:]))
-
 		// read data/branches.bin
 		data, err := Read("data/branches.bin", uint64(x))
 		if err != nil {
@@ -186,8 +146,6 @@ func main() {
 		for x := 0; x < 5; x++ {
 			hash := [32]byte{}
 			copy(hash[:], data[x*32:(x+1)*32])
-			revHash := helpers.Reverse(hash)
-			fmt.Println("hash: ", hex.EncodeToString(revHash[:]))
 			// prepend
 			pathos = append([]models.Hash{hash}, pathos...)
 		}
